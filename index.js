ce02e3ad1451cb01b2633a2bffe1f754c92824f6
[AHASSOUNI] <[nadia.ahassouni@pictime.com]>
2018-07-09 14:51:58 +0200

    make url to api

--- index.js
+++ index.js
@@ -3,27 +3,49 @@ const cors = require('cors')
 const server = express()
 const axios = require('axios');
 
+var bodyParser = require('body-parser');
+server.use(bodyParser.json());
+
+server.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 server.use(cors({ origin: 'http://localhost:4200' , credentials :  true}))
 
+
+
 server.post('/chatbotApi', (req, res) => {
-    console.log("everything ok ")
-        var filtres = []
-        // get request options
+
+        console.log(req.body)
+        let params = req.body
+        let paramsUrl = ""
+
+        for (var value in params){
+            console.log(value)
+            paramsUrl+=value+"+"
+        }
+        var baseURL = 'https://nocibe.rec.moc1.pictime.fr/ws-mobilite/search/full?searchKey='
+
+        baseURL+=paramsUrl
+
+        console.log(baseURL)
+
         const options = {
-            url:'https://nocibe.rec.moc1.pictime.fr/ws-mobilite/search/full?searchKey=channel',
+            url:baseURL,
             method: 'GET',
             headers: {
               'apiKey': '765EEABF6D18DA358B543A6E16F48'
           }
-        }
-        console.log(res)
-    
+        }    
+
+        response = {
+            url : baseURL
+            };
+        res.end(JSON.stringify(response));
         return axios(options)
         .then(response => {
-          console.log(response.data.filtres)
         });
-
+        
 });
 
 server.listen((process.env.PORT || 8000), () => {
